0d9fb2d3baf66f5fe8eb3bec32cf078b0a20894b
Julien Merlin <julienmerlin.dev@gmail.com>
2021-07-28 06:57:12 +0200

    Modification contenu About

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -7,15 +7,15 @@
     <fadeIn>
       <p>
         16pads is a collection of free MPC sound packs for music production and
-        live. All the packs are royalty free and can be use in personnal or
-        commercial projects. These packs have been created by combining multiple
-        existing royalty free sound packs on the web. The original idea was to
-        create an interactive way of sharing sounds packs for music production.
-        You can browse different packs in collection section and listen sounds
-        by clicking on pads. This project is mainly a concept for my portfolio
-        but feel free to contact me if you would love to see new packs
-        published, give me suggestions to improve theme, report a bug or give me
-        any advice you think is useful.
+        live. All the packs are free and can be use in personnal or commercial
+        projects. These packs have been created with existing royalty free
+        sounds find on the web. The original idea was to create an interactive
+        way of sharing sounds packs for music production. You can browse
+        different packs in collection section and listen sounds by clicking on
+        pads. This project is mainly a concept for my portfolio but feel free to
+        contact me if you would love to see new packs published, give me
+        suggestions to improve theme, report a bug or give me any advice you
+        think is useful.
       </p>
     </fadeIn>
     <fadeIn>
